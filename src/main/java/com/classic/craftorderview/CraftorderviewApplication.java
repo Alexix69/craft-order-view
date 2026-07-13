438bfe1a08588f0ad81c3534f9caa419bf402157
Alexis Yepez <alexixyepex@gmail.com>
2026-07-13 17:57:28 -0500

    Initialize Spring Boot project with Maven wrapper and basic configuration

---
+++ src/main/java/com/classic/craftorderview/CraftorderviewApplication.java
@@ -0,0 +1,13 @@
+package com.classic.craftorderview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CraftorderviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CraftorderviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/classic/craftorderview/CraftorderviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.classic.craftorderview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CraftorderviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
